fdc9b3d8a4664c4482b6a49f98a1484ed500ea12
Philipp <philipp_repke@gmx.de>
2026-04-02 10:39:45 +0200

    Model structure in models.py for tabels

---
+++ models.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+
+class Movie(db.Model):
+    __tablename__ = "movies"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=True)
+    director = db.Column(db.String, nullable=True)
+    poster = db.Column(db.String, nullable=False, default="/static/no-poster.png")
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship('User', backref='movies')
+
