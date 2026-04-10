e3dfeb873cc1f0160c5c12fdd4e6f1abec401a54
Philipp <philipp_repke@gmx.de>
2026-04-10 11:29:57 +0200

    debugg index.html and app.py with empty or wrong inputs from omdb api

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import requests
 from data_manager import DataManager
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for, abort
 from dotenv import load_dotenv
 from models import db, Movie
 
@@ -26,27 +26,39 @@ def get_movie_content(movie, user_id):
         "apikey": API_KEY,
         "t": movie
     }
+    errors = []
+    response = requests.get(url, params=params)
+    movie_content = response.json()
+    title = movie_content['Title']
+    year_raw = movie_content.get('Year', "")
     try:
-        response = requests.get(url, params=params)
-        movie_content = response.json()
-        title = movie_content['Title']
-        year = int(movie_content['Year'])
-        director = movie_content['Director']
-        poster = movie_content['Poster']
-        rating = float(movie_content['imdbRating'])
-        genre =  movie_content['Genre']
-        content = movie_content['Plot']
-        new_movie = Movie(title=title,
-                           year=year,
-                           director=director,
-                           poster=poster,
-                           rating=rating,
-                           genre=genre,
-                           content=content,
-                           user_id=user_id)
-    except Exception as e:
-        app.logger.error(f"API Fehler: {e}")
-        return None
+        year= int(year_raw[:4])
+    except:
+        year = None
+        errors.append("Year doesn´t exist")
+    director = movie_content['Director']
+    poster = movie_content.get('Poster')
+    if poster == "N/A":
+        poster = None
+        errors.append("Poster doesn´t exist")
+    rating_raw = movie_content.get('imdbRating', "")
+    try:
+        rating = float(rating_raw)
+    except:
+        rating = None
+        errors.append("Rating doesn´t exist")
+    genre =  movie_content['Genre']
+    content = movie_content['Plot']
+    new_movie = Movie(title=title,
+                       year=year,
+                       director=director,
+                       poster=poster,
+                       rating=rating,
+                       genre=genre,
+                       content=content,
+                       user_id=user_id)
+    if errors:
+        print("OMDB Problems:", errors)
     return new_movie
 
 @app.route('/')
@@ -57,8 +69,8 @@ def index():
     return render_template('index.html', users=users)
 
 
-@app.route('/create_user', methods=['POST'])
-def create_user():
+@app.route('/users', methods=['POST'])
+def users():
     """Wenn der Nutzer das „Nutzer hinzufügen“-Formular abschickt, wird eine POST-Anfrage ausgelöst.
     Der Server erhält die neuen Nutzerdaten, fügt sie der Datenbank hinzu und leitet dann zurück zu /.
     """
@@ -73,6 +85,10 @@ def create_user():
 def list_movies(user_id):
     """Wenn du auf einen Nutzernamen klickst, ruft die App die Liste der
     Lieblingsfilme dieses Nutzers ab und zeigt sie an."""
+    users = data_manager.get_users()
+    user = next((user for user in users if user.id == user_id), None)
+    if not user:
+        abort(404)
     movies = data_manager.get_movies(user_id)
     return render_template('movies.html', movies=movies, user_id=user_id)
 
@@ -91,6 +107,10 @@ def add_movie(user_id):
 def update_title(user_id, movie_id):
     """Den Titel eines bestimmten Films in der Liste eines Nutzers ändern,
     ohne sich auf OMDb für Korrekturen zu verlassen."""
+    movies = data_manager.get_movies(user_id)
+    movie = next((movie for movie in movies if movie.id == movie_id), None)
+    if not movie:
+        abort(404)
     updated_title = request.form.get('update_title')
     data_manager.update_movie(movie_id, updated_title)
     return redirect(url_for('list_movies', movie_id=movie_id, user_id=user_id))
@@ -98,6 +118,10 @@ def update_title(user_id, movie_id):
 @app.route('/users/<int:user_id>/movies/<int:movie_id>/delete', methods=['POST'])
 def delete_movie(user_id, movie_id):
     """Entfernt einen bestimmten Film aus der Liste der Lieblingsfilme eines Nutzers."""
+    movies = data_manager.get_movies(user_id)
+    movie = next((movie for movie in movies if movie.id == movie_id), None)
+    if not movie:
+        abort(404)
     data_manager.delete_movie(movie_id)
     return redirect(url_for('list_movies', movie_id=movie_id, user_id=user_id))
 
